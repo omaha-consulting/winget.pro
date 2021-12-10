546febab4a73eb08ebda8b72266627fccda4e04d
Michael Herrmann <michael@herrmann.io>
2021-12-10 09:20:10 +0100

    Improve formatting

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,10 +1,12 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
+
 class APITest(TestCase):
     def setUp(self):
         super().setUp()
         self.client = Client()
+
     def test_information_required_parts(self):
         response = self._get('information')
         self.assertEqual(200, response.status_code)
@@ -14,15 +16,19 @@ class APITest(TestCase):
             data['SourceIdentifier']
         )
         self.assertEqual(data['ServerSupportedVersions'], ['1.1.0'])
+
     def test_search_all_empty(self):
         response = self._post_json(
             'manifestSearch', {'FetchAllManifests': True}
         )
         self.assertEqual(200, response.status_code)
         self.assertEqual({'Data': []}, response.json())
+
     def _get(self, url_name):
         return self.client.get(reverse(f'api:{url_name}'))
+
     def _post_json(self, url_name, data):
         return self._post(url_name, data, content_type='application/json')
+
     def _post(self, url_name, *args, **kwargs):
         return self.client.post(reverse(f'api:{url_name}'), *args, **kwargs)
--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,5 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path(view, getattr(views, view), name=view)
-    for view in views.__all__
+    path(view, getattr(views, view), name=view) for view in views.__all__
 ]
