a69d81e3cacd38af25c6d7828b100ad237849406
Michael Herrmann <michael@herrmann.io>
2021-12-10 09:42:47 +0100

    Simplify code

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -3,32 +3,30 @@ from django.urls import reverse
 
 
 class APITest(TestCase):
+
     def setUp(self):
         super().setUp()
         self.client = Client()
 
     def test_information_required_parts(self):
-        response = self._get('information')
-        self.assertEqual(200, response.status_code)
-        data = response.json()['Data']
+        data = self._get('information')['Data']
         self.assertEqual(
             'github.com/omaha-consulting/winget-private-repository',
             data['SourceIdentifier']
         )
-        self.assertEqual(data['ServerSupportedVersions'], ['1.1.0'])
+        self.assertEqual(['1.1.0'], data['ServerSupportedVersions'])
 
     def test_search_all_empty(self):
-        response = self._post_json(
-            'manifestSearch', {'FetchAllManifests': True}
-        )
-        self.assertEqual(200, response.status_code)
-        self.assertEqual({'Data': []}, response.json())
+        data = self._post('manifestSearch', {'FetchAllManifests': True})['Data']
+        self.assertEqual([], data)
 
-    def _get(self, url_name):
-        return self.client.get(reverse(f'api:{url_name}'))
+    def _get(self, url_name, expect_status=200):
+        response = self.client.get(reverse(f'api:{url_name}'))
+        self.assertEqual(expect_status, response.status_code)
+        return response.json()
 
-    def _post_json(self, url_name, data):
-        return self._post(url_name, data, content_type='application/json')
-
-    def _post(self, url_name, *args, **kwargs):
-        return self.client.post(reverse(f'api:{url_name}'), *args, **kwargs)
+    def _post(self, url_name, data, expect_status=200):
+        path = reverse(f'api:{url_name}')
+        response = self.client.post(path, data, content_type='application/json')
+        self.assertEqual(expect_status, response.status_code)
+        return response.json()
