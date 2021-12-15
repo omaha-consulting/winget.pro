329de063f4e91712625a6c5b1a6a41faee064295
Michael Herrmann <michael@herrmann.io>
2021-12-15 07:52:33 +0100

    Fix `release` desk command

---
+++ bin/git-update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd /srv
+git pull
+venv/bin/pip install -U pip | grep -v "^Requirement already satisfied"
+venv/bin/pip install -Ur requirements/base-full.txt | grep -v "^Requirement already satisfied"
+su -c '/srv/bin/manage.sh migrate | grep -v "INFO\|DEBUG"' - django
+su -c '/srv/bin/manage.sh collectstatic --noinput | grep -v "INFO\|DEBUG"' - django
+
+echo `date +%H:%M:%S`': Reloading web server...'
+
+# Reload gunicorn:
+supervisorctl status gunicorn | sed "s/.*[pid ]\([0-9]\+\)\,.*/\1/" | xargs kill -HUP
+
+# Reload Nginx:
+nginx -s reload
+
+echo `date +%H:%M:%S`': Web server reload complete.'
