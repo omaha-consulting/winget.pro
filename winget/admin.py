45001161f95d98e6e18d7615ead5e37c0049911e
Michael Herrmann <michael@herrmann.io>
2022-09-21 10:25:34 +0200

    Show tenant in Package and Version superuser admin

--- winget/admin.py
+++ winget/admin.py
@@ -15,23 +15,28 @@ class PackageAdmin(ModelAdmin):
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
-        if self._is_from_superuser(request):
+        if _is_from_superuser(request):
             return qs
         return qs.filter(tenant__user=request.user)
 
     def get_exclude(self, request, obj=None):
         result = super().get_exclude(request, obj)
-        if not self._is_from_superuser(request):
+        if not _is_from_superuser(request):
             result = list(result or []) + ['tenant']
         return result
 
     def save_model(self, request, obj, form, change):
-        if not self._is_from_superuser(request):
+        if not _is_from_superuser(request):
             obj.tenant = Tenant.objects.get(user=request.user)
         super().save_model(request, obj, form, change)
 
-    def _is_from_superuser(self, request):
-        return request.user.is_superuser
+    def changelist_view(self, request, extra_context=None):
+        if _is_from_superuser(request):
+            self.list_display = ['tenant'] + list(PackageAdmin.list_display)
+            self.list_display_links = self.list_display
+            self.list_filter = ['tenant'] + list(PackageAdmin.list_filter)
+        return super().changelist_view(request, extra_context)
+
 
 class InstallerForm(ModelForm):
     class Meta:
@@ -83,6 +88,16 @@ class VersionAdmin(ModelAdmin):
                 Package.objects.filter(tenant__user=request.user)
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
+    def changelist_view(self, request, extra_context=None):
+        if _is_from_superuser(request):
+            self.list_filter = ['package__tenant'] + list(VersionAdmin.list_filter)
+            # Would also like to add package__tenant to list_display and
+            # list_display_links. Unfortunately, Django doesn't support it.
+        return super().changelist_view(request, extra_context)
+
+
+def _is_from_superuser(request):
+    return request.user.is_superuser
 
 admin.site.register(Package, PackageAdmin)
 admin.site.register(Version, VersionAdmin)
