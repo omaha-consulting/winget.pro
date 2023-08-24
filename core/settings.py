8d495a3a83c3ff563417604091fda1b6676701e3
Michael Herrmann <michael@herrmann.io>
2023-08-24 09:16:57 -0700

    Fix problem with setting AWS_S3_PROXIES

--- core/settings.py
+++ core/settings.py
@@ -2,6 +2,7 @@ from django.conf import global_settings
 from core.util import get_bool_from_env
 from pathlib import Path
 
+import json
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -111,14 +112,15 @@ DEFAULT_FILE_STORAGE = os.getenv('DEFAULT_FILE_STORAGE') or \
                        global_settings.DEFAULT_FILE_STORAGE
 
 if DEFAULT_FILE_STORAGE == 'storages.backends.s3boto3.S3Boto3Storage':
-    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    AWS_S3_HOST = os.getenv('AWS_S3_HOST') or 's3.amazonaws.com'
-    AWS_S3_ENDPOINT_URL = 'https://' + AWS_S3_HOST
-    AWS_DEFAULT_ACL = os.getenv('AWS_DEFAULT_ACL')
-    AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN')
-    AWS_S3_PROXIES = os.getenv('AWS_S3_PROXIES')
+    # AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    # AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    # AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    # AWS_S3_HOST = os.getenv('AWS_S3_HOST') or 's3.amazonaws.com'
+    # AWS_S3_ENDPOINT_URL = 'https://' + AWS_S3_HOST
+    # AWS_DEFAULT_ACL = os.getenv('AWS_DEFAULT_ACL')
+    # AWS_S3_CUSTOM_DOMAIN = os.getenv('AWS_S3_CUSTOM_DOMAIN')
+    if os.getenv('AWS_S3_PROXIES'):
+        AWS_S3_PROXIES = json.loads(os.getenv('AWS_S3_PROXIES'))
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
