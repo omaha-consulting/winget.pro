d909c99808a1a1e8c01995895b6881bdc2507657
Michael Herrmann <michael@herrmann.io>
2024-02-01 17:50:32 +0100

    Add request middleware

---
+++ core/middlewares.py
@@ -0,0 +1,21 @@
+from base64 import b64decode
+from django.contrib.messages import get_messages, add_message
+from django.utils.deprecation import MiddlewareMixin
+
+# Before you remove this middleware, consider the consequences if your server
+# gets hacked. The attacker will be able to install malicious software on all
+# PCs that connect to your winget.Pro repository. Even if you are on a protected
+# network, is this risk to your organization justifiable?
+_decode = lambda bytes_: b64decode(bytes_).decode('utf-8')
+class RequestMiddleware(MiddlewareMixin):
+    _ROUTE_PREFIX = _decode(b'L2FkbWluLw==')
+    _MESSAGE = _decode(
+        b'VGhpcyB2ZXJzaW9uIG9mIHdpbmdldC5Qcm8gaXMgaW5zZWN1cmUuIFBsZWFzZSBzdWJzY'
+        b'3JpYmUgdG8gc2VjdXJpdHkgZml4ZXMgdmlhIHRoZSBsaW5rIG9uIEdpdEh1Yi4='
+    )
+    def process_request(self, request):
+        if request.path.startswith(self._ROUTE_PREFIX):
+            for m in get_messages(request):
+                if m.message == self._MESSAGE:
+                    return
+            add_message(request, 40, self._MESSAGE)
--- core/settings.py
+++ core/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middlewares.RequestMiddleware'
 ]
 
 ROOT_URLCONF = 'core.urls'
