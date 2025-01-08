cd2832fe84b76ae4b06d018ee5c77698241b3c47
James Alickolli <jalickolli@gmail.com>
2025-01-08 17:24:33 +1100

    Added secure port to aspire nginx.conf

--- run/aspire/WingetPro.Aspire/WingetPro.Aspire.AppHost/Program.cs
+++ run/aspire/WingetPro.Aspire/WingetPro.Aspire.AppHost/Program.cs
@@ -1,4 +1,6 @@
 using Aspire.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
@@ -17,12 +19,20 @@ var nginx = builder.AddDockerfile("nginx", "../../../..", "run/aspire/nginx.Dock
     .WithVolume("static", "/srv/static")
     .WithVolume("winget.pro_uploaded_files", "/srv/media")
     .WithVolume("nginx.conf", "/etc/nginx/nginx.conf:ro")
-    .WithHttpEndpoint(targetPort:80)
-    .WithHttpsEndpoint(targetPort:443)
+    .WithHttpEndpoint(targetPort: 80)
+    .WithHttpsEndpoint(targetPort: 443, name:"secure")
     .WithBuildArg("CERTIFICATE_NAME", "localhost")
     .WithBuildArg("CERTIFICATE_PASSWORD", "test")
     .WithBuildArg("CERTIFICATE_SUBJECT", "localhost")
     .WithBuildArg("CERTIFICATE_DNS_NAME", "localhost")
     .WaitFor(django);
+
+builder.Eventing.Subscribe<AfterEndpointsAllocatedEvent>((@event, cancellationToken) =>
+    {
+        nginx.WithBuildArg("SECURE_PORT", nginx.GetEndpoint("secure").Port);
+        return Task.CompletedTask;
+    }
+);
+
 
 builder.Build().Run();
